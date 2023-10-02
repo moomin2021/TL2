d5c37cf4fa433a8ad275295527e4ae520b1efbd7
tamura_fumiya <k021g1134@g.neec.ac.jp>
2023-10-02 15:17:32 +0900

    完了

---
+++ TextureConverter.cpp
@@ -0,0 +1,37 @@
+#include "TextureConverter.h"
+#include <DirectXTex.h>
+
+void TextureConverter::ConvertTextureWICToDDS(const std::string& filePath)
+{
+	// テクスチャファイルを読み込む
+	LoadWICTextureFromFile(filePath);
+
+#pragma region DDS形式に変換して書き出す
+
+#pragma endregion
+}
+
+void TextureConverter::LoadWICTextureFromFile(const std::string& filePath)
+{
+	// ファイルパスをワイド文字列に変換する
+	std::wstring wFilePath = ConvertMultiByteStringToWideString(filePath);
+
+#pragma region テクスチャを読み込む
+
+#pragma endregion
+}
+
+std::wstring TextureConverter::ConvertMultiByteStringToWideString(const std::string& mString)
+{
+	// ワイド文字列に変換した際の文字数を計算
+	uint16_t filePathBufferSize = MultiByteToWideChar(CP_ACP, 0, mString.c_str(), -1, nullptr, 0);
+
+	// ワイド文字列
+	std::wstring wString;
+	wString.resize(filePathBufferSize);
+
+	// ワイド文字列に変換
+	MultiByteToWideChar(CP_ACP, 0, mString.c_str(), -1, &wString[0], filePathBufferSize);
+
+	return wString;
+}
--- main.cpp
+++ main.cpp
@@ -1,16 +1,34 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstdint>
+#include <DirectXTex.h>
+//#include <cassert>
+
+#include "TextureConverter.h"
+
+// コマンドライン引数
+enum Argument {
+	kApplicationPath,	// アプリケーションパス
+	kFilePath,			// 渡されたファイルのパス
+
+	NumArgument
+};
 
 int main(uint16_t argc, char* argv[]) {
-	// argcの数だけ繰り返す
-	for (uint16_t i = 0; i < argc; i++) {
-		// 文字列argvののi番を表示
-		printf(argv[i]);
+	assert(argc >= NumArgument);
 
-		// 改行
-		printf("\n");
-	}
+	// COMライブラリの初期化
+	HRESULT hr = CoInitializeEx(nullptr, COINIT_MULTITHREADED);
+	assert(SUCCEEDED(hr));
+
+	// テクスチャコンバータ
+	TextureConverter converter;
+
+	// テクスチャ変換
+	converter.ConvertTextureWICToDDS(argv[kFilePath]);
+
+	// COMライブラリの終了
+	CoUninitialize();
 
 	system("pause");
 	return 0;
