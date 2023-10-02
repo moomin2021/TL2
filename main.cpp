2128c664d0c2eef6807f0b0493eea58981511946
tamura_fumiya <k021g1134@g.neec.ac.jp>
2023-10-02 14:32:30 +0900

    完了

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <cstdio>
 #include <cstdlib>
+#include <cstdint>
 
-int main() {
-	printf("Hello, World!\n");
+int main(uint16_t argc, char* argv[]) {
+	// argc‚Ì”‚¾‚¯ŒJ‚è•Ô‚·
+	for (uint16_t i = 0; i < argc; i++) {
+		// •¶š—ñargv‚Ì‚Ìi”Ô‚ğ•\¦
+		printf(argv[i]);
+
+		// ‰üs
+		printf("\n");
+	}
 
 	system("pause");
 	return 0;
